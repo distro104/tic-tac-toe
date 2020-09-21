cf33be5e2e16c24ee8c9b66c74ddb8aabd4c8a81
distro104 <distro104@gmail.com>
2020-09-21 01:17:25 -0300

    -

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import pygame
 from pygame.locals import *
 
+
+''' Class responsible to set the board '''
+class Board:
+	def __init__(self,size = 500, column = 3, line = 3):
+		self.size = size
+		self.column = column
+		self.line = line
+		self.array_board = []
+
+
+
+	def create_board(self):
+
+		size_cel_x = self.size // self.column
+		size_cel_y = self.size // self.line
+
+
+		for i in range(self.line):
+			self.array_board.append([])
+			for j in range(self.column):
+				self.array_board[i].append([None])
+		print (self.array_board)
+
+
 # Initializiang Pygame
 pygame.init()
 
 # Screen
-SCREEN_SIZE = 700
-COLUMN = 3
-BORDER = 10
-IMG_SIZE = ( round((SCREEN_SIZE - BORDER) / 3) , round((SCREEN_SIZE - BORDER) / 3) )
+SCREEN_SIZE = 500
+IMG_SIZE = ( round(SCREEN_SIZE / 3) , round(SCREEN_SIZE / 3) )
 screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
 pygame.display.set_caption("TicTacToe")
 
@@ -20,39 +42,22 @@ BACKGROUND = pygame.transform.scale(BACKGROUND_IMAGE, (SCREEN_SIZE,SCREEN_SIZE))
 BACKGROUND_COLOR = (0 , 0, 0)
 
 
-def start_board():
-
-	array_board = [
-		[None,None,None],
-		[None,None,None],
-		[None,None,None]
-	]
-
-	x += BORDER
-	y += BORDER
-	for i in range(len(array_board)):
-		for j in range(len(array_board[i])):
-
-			array_board[i,j] = (x , y)
-
-			x += BORDER + round((SCREEN_SIZE - (BORDER * COLUMN)) / 3)
-		y += BORDER + round((SCREEN_SIZE - (BORDER * COLUMN)) / 3)
-	return array_board
+board = Board()
+board.create_board()
 
 
-def click():
-	pass
-
-
+################################### main part ###################################
 game_runing = True
-#main part
 while game_runing:
 	
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
+	screen.blit(BACKGROUND,(0,0))
 
-		screen.blit(BACKGROUND,(0,0))
 
 
-		pygame.display.update()
+
+
+
+	pygame.display.update()
