6ed8b66fed299abb64c16e49f253d6fcc702fbd1
distro104 <distro104@gmail.com>
2020-11-01 08:54:07 -0300

    Fixed varnames and white spaces

--- board.py
+++ board.py
@@ -1,67 +1,67 @@
 import pygame
 from pygame.locals import *
 
-
 ''' Class responsible to set the board '''
+
+
 class Board:
-	def __init__(self,size = 500, column = 3,margin = 10):
-		self.board_size = size
-		self.column = column
-		self.margin = margin
-		self.array_board = []
-		self.border = ((self.column + 1) * self.margin)
-		self.element_size = (self.board_size - self.border) // self.column
-		self.create_board()
+    def __init__(self, size=500, column=3, margin=10):
+        self.board_size = size
+        self.column = column
+        self.margin = margin
+        self.array_board = []
+        self.border = ((self.column + 1) * self.margin)
+        self.element_size = (self.board_size - self.border) // self.column
+        self.create_board()
 
-	# Function responsible to create the board and define the elements inside of then.
-	def create_board(self):
-		coordx = self.margin
-		coordy = self.margin
+    # Function responsible to create the board and define the elements inside of then.
+    def create_board(self):
+        coordx = self.margin
+        coordy = self.margin
 
-		width,height = ((self.element_size,self.element_size))
+        width, height = ((self.element_size, self.element_size))
 
-		for i in range(self.column):
-			self.array_board.append([])
-			for j in range(self.column):
-				self.array_board[i].append( [0, (coordy,coordx)] )
-				coordx += (width+self.margin)
-			coordx = self.margin
-			coordy += (height+self.margin)
+        for i in range(self.column):
+            self.array_board.append([])
+            for j in range(self.column):
+                self.array_board[i].append([0, (coordy, coordx)])
+                coordx += (width + self.margin)
+            coordx = self.margin
+            coordy += (height + self.margin)
 
-	# Function responsible to plot the elements in the board
-	def plot_board(self, screen, IMG_X, IMG_O):
-		# get coordenates((x , y)) -> print (self.array_board[0][0][0])
-		# plot the image -> screen.blit(IMG_O, (0,0))
-		pygame.draw.line(screen, (100,100,100), [170, 0], [170,500], 11)
-		pygame.draw.line(screen, (100,100,100), [330, 0], [330,500], 11)
-		pygame.draw.line(screen, (100,100,100), [0, 170], [500,170], 11)
-		pygame.draw.line(screen, (100,100,100), [0, 330], [500,330], 11)
+    # Function responsible to plot the elements in the board
+    def plot_board(self, screen, IMG_X, IMG_O):
+        # get coordinates((x , y)) -> print (self.array_board[0][0][0])
+        # plot the image -> screen.blit(IMG_O, (0,0))
+        pygame.draw.line(screen, (100, 100, 100), [170, 0], [170, 500], 11)
+        pygame.draw.line(screen, (100, 100, 100), [330, 0], [330, 500], 11)
+        pygame.draw.line(screen, (100, 100, 100), [0, 170], [500, 170], 11)
+        pygame.draw.line(screen, (100, 100, 100), [0, 330], [500, 330], 11)
 
+        for x in range(len(self.array_board)):
+            for y in range(len(self.array_board[x])):
+                coordx, coordy = self.array_board[y][x][1]
+                if (self.array_board[x][y][0]) == 1:
+                    screen.blit(IMG_O, (coordx, coordy))
+                elif (self.array_board[x][y][0]) == 2:
+                    screen.blit(IMG_X, (coordx, coordy))
 
-		for x in range(len(self.array_board)):
-			for y in range(len(self.array_board[x])):
-				cordx,cordy = self.array_board[y][x][1]
-				if (self.array_board[x][y][0]) == 1:
-					screen.blit(IMG_O, (cordx,cordy))
-				elif (self.array_board[x][y][0]) == 2:
-					screen.blit(IMG_X, (cordx,cordy))
+    # Function responsible to set a element inside of the board for a player
+    # Obs: if the value referent to the player is not inform the function set the board to 0
+    def set_element(self, coordxy=((0,0)), player=0):
+        # coordx,coordy = coordxy
+        # self.array_board[coordx][coordy][0] = player
+        print(f"Coordinate X Y = {coordxy}")
+        print(f"Player = {player}")
 
-	# Function responsible to set a element inside of the board for a player
-	# Obs: if the value referent to the player is not inform the function set the board to 0
-	def set_element(self, coordxy, player = 0):
-		#coordx,coordy = coordxy
-		#self.array_board[coordx][coordy][0] = player
-		print (f"Coordenada X Y = {coordxy}")
-		print (f"Jogador = {player}")
+    # Function 
+    def find_element(self, coordxy):
+        coordx, coordy = coordxy
 
-	# Function 
-	def find_element(self,coordxy):
-		coordx,coordy = coordxy
-		
-		for x in range(len(self.array_board)):
-			for y in range(len(self.array_board[x])):
-				cordx,cordy = self.array_board[y][x][1]
-				if (self.array_board[x][y][0]) == 1:
-					screen.blit(IMG_O, (cordx,cordy))
-				elif (self.array_board[x][y][0]) == 2:
-					screen.blit(IMG_X, (cordx,cordy))
+        for x in range(len(self.array_board)):
+            for y in range(len(self.array_board[x])):
+                cordx, cordy = self.array_board[y][x][1]
+                if (self.array_board[x][y][0]) == 1:
+                    screen.blit(IMG_O, (cordx, cordy))
+                elif (self.array_board[x][y][0]) == 2:
+                    screen.blit(IMG_X, (cordx, cordy))
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pygame, random, sys, time
+import pygame
+import sys
 from pygame.locals import *
 from board import Board
 from button import Button
 
-# Initializiang Pygame
+# Initializing Pygame
 pygame.init()
 
 # Screen
@@ -11,53 +12,48 @@ SCREEN_SIZE = 500
 
 board1 = Board(SCREEN_SIZE)
 
-IMG_SIZE = ( board1.element_size , board1.element_size )
+IMG_SIZE = (board1.element_size, board1.element_size)
 screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE + 50))
 pygame.display.set_caption("TicTacToe")
 
-IMG_X = pygame.transform.scale(pygame.image.load("images/x.png"),IMG_SIZE)
-IMG_O = pygame.transform.scale(pygame.image.load("images/o.png"),IMG_SIZE)
+IMG_X = pygame.transform.scale(pygame.image.load("images/x.png"), IMG_SIZE)
+IMG_O = pygame.transform.scale(pygame.image.load("images/o.png"), IMG_SIZE)
 
 BACKGROUND_IMAGE = pygame.image.load("images/background.png")
-BACKGROUND = pygame.transform.scale(BACKGROUND_IMAGE, (SCREEN_SIZE,SCREEN_SIZE))
+BACKGROUND = pygame.transform.scale(BACKGROUND_IMAGE, (SCREEN_SIZE, SCREEN_SIZE))
 
-button_dimension = (250,50)
+button_dimension = (250, 50)
 button_position = (0, 500)
-button_color = (0,50,50)
+button_color = (0, 50, 50)
 button_start = Button(screen, button_dimension, button_position, 'Start!!', button_color)
 
+# main part
+running = True
+while running:
+    player_turn = 1  # need implement rand
+    if player_turn == 1:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                running = False
+                sys.exit()
 
-################################### main part ###################################
-program_runing = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_position = pygame.mouse.get_pos()
+                board1.set_element(mouse_position, 1)
+                print(f'Player mouse position: {mouse_position}')
+                player_turn = 2
 
-while program_runing:
-	
-	player_turn = 1 # need implement rand
-	if (player_turn == 1):
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				pygame.quit()
-				program_runing = False
-				sys.exit()
+    elif player_turn == 2:
+        print('Machine turn!!')
+        player_turn = 1
 
-			if(event.type == pygame.MOUSEBUTTONDOWN):
-				mouse_position = pygame.mouse.get_pos()
-				board1.set_element(mouse_position,1)
-				print(f'Jogador clicou na posicao: {mouse_position}')
-				player_turn = 2
-				
-	elif(player_turn == 2):
-		print('Turno da maquina!!')
-		player_turn = 1
+    screen.blit(BACKGROUND, (0, 0))
 
-	
-	screen.blit(BACKGROUND,(0,0))
+    button_start.set_style('Comic Sans MS', (0, 0, 50))
 
-	button_start.set_style('Comic Sans MS',(0,0,50))
+    button_start.draw()
+    button_start.text_ajust_xy((80, 10))
+    board1.plot_board(screen, IMG_X, IMG_O)
 
-	button_start.draw()
-	button_start.text_ajust_xy((80,10))
-	board1.plot_board(screen, IMG_X, IMG_O)
-	
-
-	pygame.display.update()
+    pygame.display.update()
